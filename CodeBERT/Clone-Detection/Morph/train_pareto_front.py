d10cc9bf7f22b71932dc4e7968c23e406fa8c51e
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2025-04-15 15:26:58 +0200

    Add time record to training

--- CodeBERT/Clone-Detection/Morph/train_pareto_front.py
+++ CodeBERT/Clone-Detection/Morph/train_pareto_front.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import logging
 import csv
+import time
 
 from mo_distill_utils import distill
 
@@ -19,7 +20,7 @@ results_file = 'pareto_front_training.csv'
 fieldnames = [
         "Tokenizer", "Vocab Size", "Num Hidden Layers", "Hidden Size", "Hidden Act", "Hidden Dropout Prob",
         "Intermediate Size", "Num Attention Heads", "Attention Probs Dropout Prob", "Max Sequence Length",
-        "Position Embedding Type", "Learning Rate", "Batch Size", "Size", "Accuracy", "FLOPS", "Flips"
+        "Position Embedding Type", "Learning Rate", "Batch Size", "Size", "Accuracy", "FLOPS", "Flips", "Training Time", "Evaluation Time"
 ]
 
 # Iterate rows
@@ -27,8 +28,15 @@ for i in range(hyperparameters.shape[0]):
     # Get hyperparameters
     hyperparams = hyperparameters[i]
     objs = objs[i]
+    logging.info(f"Training {i} with FLOPs {objs[3]} and size {objs[0]}")
+    start_time = time.time()
     accs, prediction_flips = distill([hyperparams], eval=False, surrogate=False, seed=seed, weights_file=f"pareto_{i}.bin")
+    training_time = time.time()-start_time
+    logging.info(f"Training took: {training_time} seconds")
+    start_time = time.time()
     accs, prediction_flips = distill([hyperparams], eval=True, surrogate=False, seed=seed, weights_file=f"pareto_{i}.bin")
+    evaluation_time = time.time()-start_time
+    logging.info(f"Evaluation took: {evaluation_time} seconds")
 
     # Save final values to file
     with open(results_file, 'a', newline='') as file:
@@ -55,7 +63,9 @@ for i in range(hyperparameters.shape[0]):
             "Size": objs[0],  # Assuming objs[0] is the Size
             "Accuracy": accs[0],  # Assuming accs contains accuracy values
             "FLOPS": objs[3],  # Assuming objs[3] is the FLOPS
-            "Flips": prediction_flips[0]  # Assuming prediction_flips contains the flips value
+            "Flips": prediction_flips[0],  # Assuming prediction_flips contains the flips value
+            "Training Time": training_time,
+            "Evaluation Time": evaluation_time
         }
 
         # Write the row data to the CSV file
